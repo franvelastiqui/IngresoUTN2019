23e9cc449a20df17fbceeaf155117d200ff984e7
franvelastiqui <47385918+franvelastiqui@users.noreply.github.com>
2019-02-13 12:22:08 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,14 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad;
 
+edad = parseFloat(document.getElementById("edad").value);
+
+if (edad>12, edad<18)
+{
+    alert("Es adolescente");
+
+}
 
 }//FIN DE LA FUNCIÓN
