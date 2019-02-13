07d106f3e98e79bf6bc0fd3e3440b6dffdfc4658
franvelastiqui <47385918+franvelastiqui@users.noreply.github.com>
2019-02-13 12:13:43 -0300

    Add files via upload

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,39 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var ancho, largo, perimetro, alambre;
+
+    largo = parseFloat(document.getElementById("Largo").value);
+    ancho = parseFloat(document.getElementById("Ancho").value);
+
+    perimetro = largo * 2 + ancho * 2;
+    alambre = perimetro * 3;
+
+    alert("Se necesitan " + alambre + " metros de alambre.");
+
 
 }
 function Circulo () 
 {
-	
+	var radio, perimetro, alambre;
+
+    radio = parseFloat(document.getElementById("Radio").value);
+
+    perimetro = 2 * Math.PI * radio;
+    alambre = perimetro * 3;
+
+    alert("Se necesitan " + alambre + " metros de alambre.");
 }
 function Materiales () 
 {
-	
+    var ancho, largo, cemento, cal, superficie;
+
+    largo = parseFloat(document.getElementById("Largo").value);
+    ancho = parseFloat(document.getElementById("Ancho").value);
+
+    superficie = largo * ancho;
+    cal = superficie * 3;
+    cemento = superficie * 2;
+
+    alert("Para " + superficie + " m2, se necesitan " + cal + " bolsas de cal y " + cemento + " bolsas de cemento.");
 }
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,45 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+var productoUno, productoDos, productoTres, suma;
+
 function Sumar () 
 {
-	
+	productoUno = parseFloat(document.getElementById("PrecioUno").value);
+    productoDos = parseFloat(document.getElementById("PrecioDos").value);
+    productoTres = parseFloat(document.getElementById("PrecioTres").value);
+
+    suma = productoUno + productoDos + productoTres;
+
+    alert("La suma de los productos es " + suma);
 }
 function Promedio () 
 {
-	
+    var promedio
+
+	productoUno = parseFloat(document.getElementById("PrecioUno").value);
+    productoDos = parseFloat(document.getElementById("PrecioDos").value);
+    productoTres = parseFloat(document.getElementById("PrecioTres").value);
+
+    suma = productoUno + productoDos + productoTres;
+    promedio = suma / 3
+
+    alert("El promedio de los productos es " + promedio);
 }
 function PrecioFinal () 
 {
-	
+    var porcentaje, precioFinal
+
+	productoUno = parseFloat(document.getElementById("PrecioUno").value);
+    productoDos = parseFloat(document.getElementById("PrecioDos").value);
+    productoTres = parseFloat(document.getElementById("PrecioTres").value);
+
+    suma = productoUno + productoDos + productoTres;
+    porcentaje = suma * 21 / 100;
+    precioFinal = suma + porcentaje;
+
+    alert("El precio final es " + precioFinal);
+
+
 }
