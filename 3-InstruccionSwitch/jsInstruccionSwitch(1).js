90a6fa963959da7eb1ecea5b748571114d5228bf
franvelastiqui <47385918+franvelastiqui@users.noreply.github.com>
2019-02-20 12:05:08 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,23 @@
 function Mostrar()
 {
 //tomo el mes
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
-
+switch (mesDelAnio)
+{
+    case "Enero":
+        alert("Que comiences bien el año!");
+        break;
+    case "Marzo":
+        alert("A clases");
+        break;
+    case "Julio":
+        alert("Que comiencen las vacaciones");
+        break;
+    case "Diciembre":
+        alert("Felices fiestas");
+        break;
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,18 @@
 function Mostrar()
 {
 //tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAnio = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+//alert (mesDelAnio);
 	
-	
-
+switch (mesDelAnio)
+{
+    case "Febrero":
+        alert("Este mes no tiene mas de 29 dias");
+        break;
+    default:
+        alert("Este mes tiene 30 dias o mas");
+        break;
+}
 
 }//FIN DE LA FUNCIÓN
